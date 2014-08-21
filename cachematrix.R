1d9a7efbbdead9a74fc0dc6fa295f6c22d13eaee
amimiz <ami.mizrachi+github@gmail.com>
2014-08-21 23:47:23 +0300

    Commented Solution Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,30 @@
 ## Solve Inverse Matrix and do it only once - using cache
-## [Put comments here that describe what your functions do]
+
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-                x <<- y
+        m <- NULL    # new vector - set the solution cache as null
+        set <- function(y) {              # set the new vector - usage "x$set(matrix)"
+                x <<- y      
                 m <<- NULL
         }
-        get <- function() x
-        setsolve <- function(solve) m <<- solve
-        getsolve <- function() m
+        get <- function() x               # get the vector - usage "x$get()
+        setsolve <- function(solve) m <<- solve # set the solution - for internal use only (used by cacheSolve() function
+        getsolve <- function() m          # get the inverse matrix (if solved - otherwise will return NULL) - usage "x$getsolve()"
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)       
 }
 
-
-cacheSolve <- function(x, ...) {
-        m <- x$getsolve()
+# cacheSolve function calculates the Inverse Matrix, only if it is not solved already (cached) 
+cacheSolve <- function(x, ...) { 
+        m <- x$getsolve()         #Checks if solved already
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
-        data <- x$get()
+        data <- x$get()          #If not solved - solve it
         m <- solve(data, ...)
-        x$setsolve(m)
+        x$setsolve(m)           # and store it in Cache
         m
         ## Return a matrix that is the inverse of 'x'
 }
